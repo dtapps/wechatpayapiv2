32eae38104d7e63fc9173b9247dff95747844c29
dtapps <li.gc@icloud.com>
2024-03-08 11:56:33 +0800

    - update

--- pay.unifiedorder.notify.http.go
+++ pay.unifiedorder.notify.http.go
@@ -8,34 +8,34 @@ import (
 
 // PayUnifiedOrderNotifyHttpRequest 小程序支付 - 统一下单 - 回调通知 - 请求参数
 type PayUnifiedOrderNotifyHttpRequest struct {
-	ReturnCode         string `json:"return_code" xml:"return_code"`                   // 返回状态码
-	ReturnMsg          string `json:"return_msg" xml:"return_msg"`                     // 返回信息
-	Appid              string `json:"appid" xml:"appid"`                               // 小程序ID
-	MchId              string `json:"mch_id" xml:"mch_id"`                             // 商户号
-	DeviceInfo         string `json:"device_info" xml:"device_info"`                   // 设备号
-	NonceStr           string `json:"nonce_str" xml:"nonce_str"`                       // 随机字符串
-	Sign               string `json:"sign" xml:"sign"`                                 // 签名
-	SignType           string `json:"sign_type" xml:"sign_type"`                       // 签名类型
-	ResultCode         string `json:"result_code" xml:"result_code"`                   // 业务结果
-	ErrCode            string `json:"err_code" xml:"err_code"`                         // 错误代码
-	ErrCodeDes         string `json:"err_code_des" xml:"err_code_des"`                 // 错误代码描述
-	Openid             string `json:"openid" xml:"openid"`                             // 用户标识
-	IsSubscribe        string `json:"is_subscribe" xml:"is_subscribe"`                 // 是否关注公众账号
-	TradeType          string `json:"trade_type" xml:"trade_type"`                     // 交易类型
-	BankType           string `json:"bank_type" xml:"bank_type"`                       // 付款银行
-	TotalFee           int    `json:"total_fee" xml:"total_fee"`                       // 订单金额
-	SettlementTotalFee int    `json:"settlement_total_fee" xml:"settlement_total_fee"` // 应结订单金额
-	FeeType            string `json:"fee_type" xml:"fee_type"`                         // 货币种类
-	CashFee            int    `json:"cash_fee" xml:"cash_fee"`                         // 现金支付金额
-	CashFeeType        string `json:"cash_fee_type" xml:"cash_fee_type"`               // 现金支付货币类型
-	CouponFee          string `json:"coupon_fee" xml:"coupon_fee"`                     // 总代金券金额
-	CouponCount        int    `json:"coupon_count" xml:"coupon_count"`                 // 代金券使用数量
-	CouponType         string `json:"coupon_type" xml:"coupon_type"`                   // 代金券类型
-	CouponId           string `json:"coupon_id" xml:"coupon_id"`                       // 代金券ID
-	TransactionId      string `json:"transaction_id" xml:"transaction_id"`             // 微信支付订单号
-	OutTradeNo         string `json:"out_trade_no" xml:"out_trade_no"`                 // 商户订单号
-	Attach             string `json:"attach" xml:"attach"`                             // 商家数据包
-	TimeEnd            string `json:"time_end" xml:"time_end"`                         // 支付完成时间
+	ReturnCode         string `json:"return_code" xml:"return_code"`                                       // 返回状态码
+	ReturnMsg          string `json:"return_msg,omitempty" xml:"return_msg,omitempty"`                     // 返回信息
+	Appid              string `json:"appid,omitempty" xml:"appid,omitempty"`                               // 小程序ID
+	MchId              string `json:"mch_id,omitempty" xml:"mch_id,omitempty"`                             // 商户号
+	DeviceInfo         string `json:"device_info,omitempty" xml:"device_info,omitempty"`                   // 设备号
+	NonceStr           string `json:"nonce_str,omitempty" xml:"nonce_str,omitempty"`                       // 随机字符串
+	Sign               string `json:"sign,omitempty" xml:"sign,omitempty"`                                 // 签名
+	SignType           string `json:"sign_type,omitempty" xml:"sign_type,omitempty"`                       // 签名类型
+	ResultCode         string `json:"result_code,omitempty" xml:"result_code,omitempty"`                   // 业务结果
+	ErrCode            string `json:"err_code,omitempty" xml:"err_code,omitempty"`                         // 错误代码
+	ErrCodeDes         string `json:"err_code_des,omitempty" xml:"err_code_des,omitempty"`                 // 错误代码描述
+	Openid             string `json:"openid,omitempty" xml:"openid,omitempty"`                             // 用户标识
+	IsSubscribe        string `json:"is_subscribe,omitempty" xml:"is_subscribe,omitempty"`                 // 是否关注公众账号
+	TradeType          string `json:"trade_type,omitempty" xml:"trade_type,omitempty"`                     // 交易类型
+	BankType           string `json:"bank_type,omitempty" xml:"bank_type,omitempty"`                       // 付款银行
+	TotalFee           int    `json:"total_fee,omitempty" xml:"total_fee,omitempty"`                       // 订单金额
+	SettlementTotalFee int    `json:"settlement_total_fee,omitempty" xml:"settlement_total_fee,omitempty"` // 应结订单金额
+	FeeType            string `json:"fee_type,omitempty" xml:"fee_type,omitempty"`                         // 货币种类
+	CashFee            int    `json:"cash_fee,omitempty" xml:"cash_fee,omitempty"`                         // 现金支付金额
+	CashFeeType        string `json:"cash_fee_type,omitempty" xml:"cash_fee_type,omitempty"`               // 现金支付货币类型
+	CouponFee          string `json:"coupon_fee,omitempty" xml:"coupon_fee,omitempty"`                     // 总代金券金额
+	CouponCount        int    `json:"coupon_count,omitempty" xml:"coupon_count,omitempty"`                 // 代金券使用数量
+	CouponType         string `json:"coupon_type,omitempty" xml:"coupon_type,omitempty"`                   // 代金券类型
+	CouponId           string `json:"coupon_id,omitempty" xml:"coupon_id,omitempty"`                       // 代金券ID
+	TransactionId      string `json:"transaction_id,omitempty" xml:"transaction_id,omitempty"`             // 微信支付订单号
+	OutTradeNo         string `json:"out_trade_no,omitempty" xml:"out_trade_no,omitempty"`                 // 商户订单号
+	Attach             string `json:"attach,omitempty" xml:"attach,omitempty"`                             // 商家数据包
+	TimeEnd            string `json:"time_end,omitempty" xml:"time_end,omitempty"`                         // 支付完成时间
 }
 
 // PayUnifiedOrderNotifyHttp 小程序支付 - 统一下单 - 回调通知
--- secapi.pay.refund.notify.http.go
+++ secapi.pay.refund.notify.http.go
@@ -8,12 +8,12 @@ import (
 
 // SecApiPayRefundNotifyHttpRequest 小程序支付 - 申请退款 - 回调通知 - 请求参数
 type SecApiPayRefundNotifyHttpRequest struct {
-	ReturnCode string `json:"return_code" xml:"return_code"` // 返回状态码
-	ReturnMsg  string `json:"return_msg" xml:"return_msg"`   // 返回信息
-	Appid      string `json:"appid" xml:"appid"`             // 公众账号ID
-	MchId      string `json:"mch_id" xml:"mch_id"`           // 退款的商户号
-	NonceStr   string `json:"nonce_str" xml:"nonce_str"`     // 随机字符串
-	ReqInfo    string `json:"req_info" xml:"req_info"`       // 加密信息
+	ReturnCode string `json:"return_code" xml:"return_code"`                   // 返回状态码
+	ReturnMsg  string `json:"return_msg,omitempty" xml:"return_msg,omitempty"` // 返回信息
+	Appid      string `json:"appid,omitempty" xml:"appid,omitempty"`           // 公众账号ID
+	MchId      string `json:"mch_id,omitempty" xml:"mch_id,omitempty"`         // 退款的商户号
+	NonceStr   string `json:"nonce_str,omitempty" xml:"nonce_str,omitempty"`   // 随机字符串
+	ReqInfo    string `json:"req_info,omitempty" xml:"req_info,omitempty"`     // 加密信息
 }
 
 // SecApiPayRefundNotifyHttp 小程序支付 - 申请退款 - 回调通知
